1873f5ee82c285045e3810550a19f317b9cd800b
WR169 <YanagawaYuki@outlook.jp>
2022-04-24 19:58:14 +0900

    予定入力フォームを作成した

--- ts-1/src/App.tsx
+++ ts-1/src/App.tsx
@@ -1,16 +1,42 @@
 import './App.css'
 import Calendar, { DateCallback } from 'react-calendar';
 import './react-calendar/dist/Calendar.css';
-import { MouseEvent } from 'react';
+import { MouseEvent, MouseEventHandler, SetStateAction } from 'react';
 import { useState } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+} from '@chakra-ui/react'
+import { Input } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 
+const formatDateToString = (dt: Date): string => {
+  const y = dt.getFullYear();
+  const m = ('00' + (dt.getMonth()+1)).slice(-2);
+  const d = ('00' + dt.getDate()).slice(-2);
+  return (y + '年' + m + '月' + d + '日');
+}
 
 function App() {
   const [value, setvalue] = useState<Date>(new Date());
+  const [selectedDay, setSelectedDay] = useState<Date>(new Date())
+  const [scheduleText, setScheduleText] = useState('')
+
   const onClickCalendarCell: DateCallback | undefined = (datevalue: Date, event: MouseEvent<HTMLButtonElement>) => {
     // setvalue(datevalue)
+    setSelectedDay(datevalue)
     console.log(`day clicked: ${datevalue}`)
     console.log(`value: ${value}`)
+    
+  }
+
+  const handleInputedSchedule = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setScheduleText(e.target.value)
+  }
+
+  const handleScheduleTextSet: MouseEventHandler<HTMLButtonElement> = (e) => {
+    console.log(scheduleText)
   }
 
   return (
@@ -21,7 +47,14 @@ function App() {
         </header>
       </div>
       <div className='calendar-grid'>
-        <Calendar onClickDay={onClickCalendarCell} value={value} />
+        <Calendar onClickDay={onClickCalendarCell} value={value}/>
+      </div>
+      <div className='input-schedule-form'>
+        <FormControl>
+          <FormLabel>{formatDateToString(selectedDay)}の予定</FormLabel>
+          <Input type='text' onChange={handleInputedSchedule}/>
+          <Button colorScheme='blue' onClick={handleScheduleTextSet}>Set</Button>
+        </FormControl>
       </div>
     </>
   );
--- ts-1/src/index.tsx
+++ ts-1/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client';
+
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 
