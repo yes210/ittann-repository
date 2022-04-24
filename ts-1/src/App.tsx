e339980328d03058d8cb6aa4ce20e578ca2c6296
WR169 <YanagawaYuki@outlook.jp>
2022-04-24 16:07:55 +0900

    カレンダー表示した

--- ts-1/src/App.tsx
+++ ts-1/src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import './App.css'
+import Calendar, { DateCallback } from 'react-calendar';
+import { MouseEvent } from 'react';
+import { useState } from 'react';
+
 
 function App() {
+  const [value, setvalue] = useState<Date>(new Date());
+  const onClickCalendarCell: DateCallback | undefined = (datevalue: Date, event: MouseEvent<HTMLButtonElement>) => {
+    // setvalue(datevalue)
+    console.log(`day clicked: ${datevalue}`)
+    console.log(`value: ${value}`)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        hello,ts
-      </header>
-    </div>
+    <>
+      <div className="title">
+        <header className="App-header">
+          カレンダー
+        </header>
+      </div>
+      <div className='calendar-grid'>
+        <Calendar onClickDay={onClickCalendarCell} value={value} />
+      </div>
+    </>
   );
 }
 
